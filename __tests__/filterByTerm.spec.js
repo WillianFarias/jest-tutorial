9e42cc3f9d020b08241e977a0c2098d498f0d40f
WillianFarias <williansousafarias@gmail.com>
2020-09-30 15:26:56 -0300

    test search url and search empty

--- __tests__/filterByTerm.spec.js
+++ __tests__/filterByTerm.spec.js
@@ -11,8 +11,40 @@ describe("Filter function", () => {
     const output = [{ id: 3, url: "https://www.link3.dev" }];
 
     expect(filterByTerm(input, "link")).toEqual(output);
+    expect(filterByTerm(input, "LINK")).toEqual(output); // maisculas
+  });
 
-    expect(filterByTerm(input, "LINK")).toEqual(output); // New test
+  test("it should filter by a search term (url)", () => {
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+
+    const output = [ 
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" }
+    ];
+
+    expect(filterByTerm(input, "uRl")).toEqual(output);//pesquisando pelo termo url
+
+  });
+
+  test("empty search term", () => {
+    const input = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+
+    const output = [
+      { id: 1, url: "https://www.url1.dev" },
+      { id: 2, url: "https://www.url2.dev" },
+      { id: 3, url: "https://www.link3.dev" }
+    ];
+
+    expect(filterByTerm(input, "")).toEqual(output);
+
   });
 
 });
